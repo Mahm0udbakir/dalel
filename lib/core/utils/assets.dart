03ba1132b87beb085cbefb99b1955d13d7da3dc0
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-04-03 07:24:49 +0200

    Added Assets and colors

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,14 @@
+class Assets {
+  Assets._();
+  
+  static const String assetsImagesForgotPassword = "assets/images/ForgotPassword.jpeg";
+
+  static const String assetsImagesOnBoarding1 = "assets/images/OnBoarding1.jpeg";
+
+  static const String assetsImagesOnBoarding2 = "assets/images/OnBoarding2.jpeg";
+
+  static const String assetsImagesOnBoarding3 = "assets/images/OnBoarding3.jpeg";
+
+  static const String assetsImagesOrderSuccess = "assets/images/OrderSuccess.jpeg";
+}
+
---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0xfffc4985);
+  static Color offWhite = const Color(0xfff8f4f9);
+  static Color deepBrown = const Color(0xff6f48be);
+  static Color grey = const Color(0xffc4c4c4);
+  static Color deepGrey = const Color(0xff6f6460);
+  static Color lightGrey = const Color(0xffb4b4b4);
+  static Color black = const Color(0xff333333);
+}
