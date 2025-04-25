64bf007091cc7225dda31daba66cfc044ea90cd9
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-04-25 22:56:52 +0300

    Refactor HistoricalPeriodsSection to use typed CustomListView with HomeCubit for improved state management and data handling.

--- lib/features/home/presentation/widgets/historical_periods_section.dart
+++ lib/features/home/presentation/widgets/historical_periods_section.dart
@@ -2,9 +2,11 @@ import 'package:dalel/core/utils/strings.dart';
 import 'package:dalel/core/widgets/custom_option_item.dart';
 import 'package:dalel/core/widgets/custom_list_view.dart';
 import 'package:dalel/core/widgets/custom_text_header.dart';
+import 'package:dalel/features/home/data/model/historical_periods_model.dart';
 import 'package:dalel/features/home/presentation/view_model/cubit/home_cubit.dart';
 import 'package:dalel/features/home/presentation/view_model/cubit/home_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HistoricalPeriodsSection extends StatelessWidget {
   const HistoricalPeriodsSection({super.key});
@@ -15,9 +17,9 @@ class HistoricalPeriodsSection extends StatelessWidget {
       children: [
         const CustomTextHeader(text: MyAppStrings.historicalPeriods),
         const SizedBox(height: 16),
-        CustomListView(
+        CustomListView<HistoricalPeriodsModel, HomeCubit, HomeStates>(
+          cubit: context.read<HomeCubit>(),
           height: 96,
-          cubit: HomeCubit(),
           getData: (state) {
             if (state is HistoricalPeriodSuccessState) {
               return state.historicalPeriods;
